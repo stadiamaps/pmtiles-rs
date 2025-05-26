30040fa930f041d1f175d284427fbf345e6e25a8
Frank Elsinga <frank@elsinga.de>
2025-05-26 19:50:36 +0200

    chore: reformat code (#54)
    * reformat some things according to just fmt
    * fix doc comments on modules
    
    ---------
    
    Co-authored-by: Yuri Astrakhan <YuriAstrakhan@gmail.com>

--- src/backend_s3.rs
+++ src/backend_s3.rs
@@ -1,12 +1,10 @@
 use bytes::Bytes;
 use s3::Bucket;
 
-use crate::{
-    async_reader::{AsyncBackend, AsyncPmTilesReader},
-    cache::{DirectoryCache, NoCache},
-    error::PmtError::ResponseBodyTooLong,
-    PmtResult,
-};
+use crate::async_reader::{AsyncBackend, AsyncPmTilesReader};
+use crate::cache::{DirectoryCache, NoCache};
+use crate::error::PmtError::ResponseBodyTooLong;
+use crate::PmtResult;
 
 impl AsyncPmTilesReader<S3Backend, NoCache> {
     /// Creates a new `PMTiles` reader from a bucket and path to the
