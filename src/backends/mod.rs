a0877882edc26d53ddcb0b4b77d84b9161eef307
Frank Elsinga <frank@elsinga.de>
2025-10-12 23:00:26 +0200

    move cfgs around to be more maintainable

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -1,10 +1,20 @@
 #[cfg(feature = "aws-s3-async")]
 mod aws_s3;
+#[cfg(feature = "aws-s3-async")]
+pub use crate::backends::aws_s3::AwsS3Backend;
 #[cfg(feature = "http-async")]
 mod http;
+#[cfg(feature = "http-async")]
+pub use crate::backends::http::HttpBackend;
 #[cfg(feature = "mmap-async-tokio")]
 mod mmap;
+#[cfg(feature = "mmap-async-tokio")]
+pub use crate::backends::mmap::MmapBackend;
 #[cfg(feature = "object-store")]
 mod object_store;
+#[cfg(feature = "object-store")]
+pub use crate::backends::object_store::ObjectStoreBackend;
 #[cfg(feature = "__async-s3")]
 mod s3;
+#[cfg(feature = "__async-s3")]
+pub use crate::backends::s3::S3Backend;
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod async_reader;
 pub use async_reader::{AsyncBackend, AsyncPmTilesReader};
 
 mod backends;
+pub use backends::*;
 
 #[cfg(feature = "__async")]
 mod cache;
@@ -22,16 +23,6 @@ mod writer;
 /// Re-export of crate exposed in our API to simplify dependency management
 #[cfg(feature = "__async-aws-s3")]
 pub use aws_sdk_s3;
-#[cfg(feature = "aws-s3-async")]
-pub use backends::aws_s3::AwsS3Backend;
-#[cfg(feature = "http-async")]
-pub use backends::http::HttpBackend;
-#[cfg(feature = "mmap-async-tokio")]
-pub use backends::mmap::MmapBackend;
-#[cfg(feature = "object-store")]
-pub use backends::object_store::ObjectStoreBackend;
-#[cfg(feature = "__async-s3")]
-pub use backends::s3::S3Backend;
 #[cfg(feature = "iter-async")]
 pub use directory::DirEntryCoordsIter;
 pub use directory::{DirEntry, Directory};
